ca01f5429281144830483354aa36d6aea11fdf21
selkasse <selkasse@gmu.edu>
2020-09-07 19:47:39 -0400

    add separate CSS file and add comments to CDN and javascript

--- codemirror.js
+++ codemirror.js
@@ -1,15 +1,22 @@
+// * Set initial editor values
+
 let htmlCode = document.getElementById('htmlCode');
 htmlCode.innerHTML = `<h1 id="my-h1">Hey now!</h1>
 <button onclick="changeH1()">Click Me!</button>`;
+
 let cssCode = document.getElementById('cssCode');
 cssCode.innerHTML = `h1 {
     color: blue;
 }`;
+
 let jsCode = document.getElementById('jsCode');
 jsCode.innerHTML = `function changeH1(){
     let h1 = document.getElementById('my-h1'); 
     h1.innerHTML = 'Boom!';
 }`;
+
+// * Convert textareas to CodeMirror objects
+
 let htmlEditor = CodeMirror.fromTextArea(
     document.getElementById("htmlCode"),
     {
@@ -37,9 +44,11 @@ let jsEditor = CodeMirror.fromTextArea(
         autoCloseBrackets: true,
     }
 );
+
 let frame = document.getElementById("preview-window").contentWindow
     .document;
 
+// * Render the preview in the iframe
 function showPreview() {
     let htmlCode = htmlEditor.getValue();
     let cssCode = `<style>${cssEditor.getValue()}</style>`;
@@ -49,9 +58,11 @@ function showPreview() {
     frame.close();
 }
 
+// * Add CodeMirror event listeners to editors
 htmlEditor.on("change", showPreview);
 cssEditor.on("change", showPreview);
-
+// * Only call showPreview when the user presses a key that likely signifies they are done writing that section of code
+// * This prevents console errors when in the middle of typing new code
 jsEditor.on("keyup", function (cm, event) {
     if (
         event.key == "Enter" ||
